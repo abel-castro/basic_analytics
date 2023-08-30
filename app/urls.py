268ed1b80d87dc0d2a0232331a8e68539e3ca563
Abel Castro <hi@abelcastro.dev>
2023-08-30 12:40:54 +0200

    FIx admin statics files locally

--- app/urls.py
+++ app/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 from django.urls import path
 
@@ -66,3 +67,7 @@ urlpatterns = [
     ),
     path("api/track/", TrackView.as_view(), name="track_view"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
